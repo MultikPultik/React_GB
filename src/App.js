bb789d48cd0b885f751168b73de011df3eccf3e9
AvisRara <tsiganov.da@gmail.com>
2021-08-05 14:03:12 +0400

    lesson-2

--- src/App.js
+++ src/App.js
@@ -3,46 +3,35 @@ import { Message } from './components/message/Message.js';
 import { useState, useEffect } from "react";
 
 export function App(props) {
-  const [messages, setMessages] = useState([
+  const [messageList, setMessagesList] = useState([
     { name: 'Bot', msg: 'Привет, это Bot!' },
   ]);
 
   const [value, setValue] = useState('');
 
   function sendMessage() {
-    setMessages((state) => [...state, { name: 'Дмитрий', msg: value }]);
+    setMessagesList((state) => [...state, { name: 'Дмитрий', msg: value }]);
     setValue('');
   }
 
   useEffect(() => {
-    const prevMessage = messages[messages.length - 1];
+    const prevMessage = messageList[messageList.length - 1];
     console.log(prevMessage);
 
     if (prevMessage.name === 'Дмитрий') {
       setTimeout(() => {
-        setMessages((state) => [
+        setMessagesList((state) => [
           ...state, 
           {name: 'Bot', msg: 'Я еще не настроен... Извини не могу пообщаться :(('},
         ])
       }, 1000)
     }
-  }, [messages]);
+  }, [messageList]);
 
   return (
     <div className="App">
       <div className="App-wrap">
-        <div className="App-content">
-          {messages.map((message, id) => (
-            <div key={id} className='Wrap-msg'>
-              <div className='Header-msg'>
-                {message.name}
-              </div>
-              <div className='Content-msg'>
-                {message.msg}
-              </div>
-            </div>)
-          )}
-        </div>
+        <Message messageList = {messageList}/>
         <div className='App-footer'>
           <input
             className='Input-msg'
--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -2,6 +2,17 @@ import './message.css';
 
 export function Message(props) {
   return (
-    <p className='msg-text'>{props.text}</p>
+    <div className="App-content">
+          {props.messageList.map((message, id) => (
+            <div key={id} className='Wrap-msg'>
+              <div className='Header-msg'>
+                {message.name}
+              </div>
+              <div className='Content-msg'>
+                {message.msg}
+              </div>
+            </div>)
+          )}
+    </div>
   )
 }
