778a6b171894ef9f9d2454a3268d0ecf88ec0c78
AvisRara <tsiganov.da@gmail.com>
2021-08-16 05:52:16 +0400

    Промежуточная стадия

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import { Button } from './components/button/button';
-import { Input } from './components/input/input';
-import { Message } from './components/message/Message';
+// import './App.css';
+import { Box } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import  {React, useRef, useState, useEffect } from 'react';
+
+import { MessageLayout } from './components/messageLayout/MessageLayout';
+import { Send } from './components/send/Send';
 
 export function App() {
   const [messageList, setMessagesList] = useState([
@@ -10,38 +15,95 @@ export function App() {
   ]);
 
   const [value, setValue] = useState('');
+  const inputRef = useRef(null);
 
   const sendMessage = () => {
     setMessagesList((state) => [...state, { name: 'Дмитрий', msg: value }]);
     setValue('');
+
+    inputRef.current.focus();
   };
 
   useEffect(() => {
     const prevMessage = messageList[messageList.length - 1];
-    console.log(prevMessage);
 
+    inputRef.current.focus();
     if (prevMessage.name === 'Дмитрий') {
       setTimeout(() => {
         setMessagesList((state) => [
           ...state,
           {
             name: 'Bot',
-            msg: 'Я еще не настроен... Извини не могу пообщаться :((',
+            msg: 'Я еще не настроен... Извини, не могу пообщаться',
           },
         ]);
       }, 1000);
     }
   }, [messageList]);
-
   return (
     <div className="App">
-      <div className="App-wrap">
-        <Message messageList={messageList} />
-        <div className="App-footer">
-          <Input value={value} setValue={setValue} />
-          <Button sendMessage={sendMessage} />
-        </div>
-      </div>
+      <Container
+        maxWidth="sm"
+        style={{ backgroundColor: '#cfe8fc', height: '95vh' }}
+      >
+        <Box height="100%" bgcolor="grey.300">
+          <Box display="flex" height="100%" p={0} bgcolor="background.paper">
+            <Box flexGrow={1}>
+              <Box
+                p={0}
+                display="flex"
+                flexDirection="column"
+                height="100%"
+                bgcolor="grey.300"
+              >
+                <Box p={1} bgcolor="grey.400">
+                  Item 2
+                </Box>
+                <Box p={1} flex={1} flexGrow={1} bgcolor="grey.500">
+                  Item 3
+                </Box>
+              </Box>
+            </Box>
+
+            <Box
+              p={0}
+              flexGrow={1}
+              style={{ width: '250px' }}
+              bgcolor="grey.400"
+            >
+              <Box
+                p={0}
+                display="flex"
+                flexDirection="column"
+                height="100%"
+                bgcolor="grey.300"
+              >
+                <Box p={1} flex={0} bgcolor="grey.200">
+                  Header
+                </Box>
+                <MessageLayout messageList={messageList} />
+
+                <Box p={1} flex={0} bgcolor="grey.200">
+                  <Input
+                    value={value}
+                    inputRef={inputRef}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder="Введите сообщение..."
+                    fullWidth={true}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton onClick={sendMessage}>
+                          <Send />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
     </div>
   );
 }
--- src/components/button/button.js
+++
@@ -1,7 +0,0 @@
-export function Button(props) {
-  return (
-    <button className="Btn-msg" onClick={props.sendMessage}>
-      Отправить
-    </button>
-  );
-}
